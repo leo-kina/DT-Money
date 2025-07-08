ed7dfa6b4b4455ca73773a6f9d60f6fba4ab19d3
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-07-08 20:04:24 -0300

    adicionado useForm e zod

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { SearchFormContainer } from './styles'
 import { MagnifyingGlass } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
 
 export const SearchForm = () => {
+  const {register, handleSubmit} = useForm()
+  function handleSeachTransaction() {
+
+  }
   return (
-    <SearchFormContainer>
-        <input type="text" />
+    <SearchFormContainer onSubmit={handleSubmit(handleSeachTransaction)}>
+        <input type="text"
+        placeholder='Busque por transacoes' 
+        {...register('query')}/>
         <button type='submit'>
             <MagnifyingGlass size={20}/>
             Buscar
